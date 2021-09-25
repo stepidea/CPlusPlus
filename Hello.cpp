4d1d5dc385d8a02bdc79b1cded5909e69b799929
zebinhong <zebin_hung@outlook.com>
2021-09-25 23:55:31 +0800

    Add IO.cpp

--- Hello.cpp
+++ Hello.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+//C++ 实例 - Hello World
 int main(){
     cout<<"This is my first C++ demo."<<endl;
     cout<<"Hello,World!"<<endl;
---
+++ IO.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+//C++ 实例 - 标准输入输出
+int main(){
+    int num;
+    cout<<"Please Enter a number: ";
+    cin>>num;
+    cout<<endl<<num;
+    return 0;
+}
